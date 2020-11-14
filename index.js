e4e7d237dc3a862bae1cc59fa4b70c5ff017ef83
KristynaBartonova <kikisbartonova@gmail.com>
2020-11-14 11:35:43 +0100

    js

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 'use strict';
-
+let Answer = 'Špatně';
 let Choice = document.querySelectorAll('.tlac');
 
-for (let i = 0; i < Choice.length; i++){
- Choice[i].addEventListener('click', (event) => event.target.classList.toggle("vybrano"));
-};
-
-
-
-
-
-
+for (let i = 0; i < Choice.length; i++) {
+  Choice[i].addEventListener('click', (event) => {
+    if (Choice[i] === Choice[3]) {
+      event.target.classList.toggle('vybrano');
+      document.getElementById('div1').textContent =
+        'Výborně, tohle je správná odpověď';
+    } else {
+      event.target.classList.toggle('vybrano');
+      document.getElementById('div1').textContent = 'Tohle není správně';
+    }
+  });
+}
